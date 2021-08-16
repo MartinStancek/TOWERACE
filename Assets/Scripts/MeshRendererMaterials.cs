b303e43df48b4abe14369be712f96776b85feb3f
Martin Stanček <martin.stancek123@gmail.com>
2021-08-16 10:04:14 +0200

    added color to blackhole efect

--- Assets/Scripts/MeshRendererMaterials.cs
+++ Assets/Scripts/MeshRendererMaterials.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 [System.Serializable]
 public class MeshRendererMaterials
 {
+    public ParticleSystem particles;
     public MeshRenderer renderer;
     public List<int> materialIndexes;
 
     public void SetColor(Color color)
     {
-        foreach (var i in materialIndexes)
+        if (renderer != null)
         {
-            var matA = renderer.materials[i].color.a;
-            renderer.materials[i].color = new Color(color.r, color.g, color.b, matA);
+            foreach (var i in materialIndexes)
+            {
+                var matA = renderer.materials[i].color.a;
+                renderer.materials[i].color = new Color(color.r, color.g, color.b, matA);
+            }
         }
+        
+        if (particles != null)
+        {
+            var main = particles.main;
+            main.startColor = color;
+        }
+
     }
 }
