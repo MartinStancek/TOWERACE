2268ceafa95dfdae1b4d65a5b082074371966d2c
Martin Stanček <martin.stancek123@gmail.com>
2021-08-23 11:56:24 +0200

    fixes

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -375,8 +375,8 @@ public class GameController : MonoBehaviour
         onStartGame.Invoke();
         SetupUISnaps();
 
-        //StartRace();
-        EndRace();
+        StartRace();
+        //EndRace();
     }
 
     private void SetCarCameras(bool value)
